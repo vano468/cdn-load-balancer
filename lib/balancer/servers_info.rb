99c7c0d930dd2d396bedb62d1e5cebb07466c505
Ivan Kornilov <vano468@gmail.com>
2016-11-15 17:02:05 +0300

    Fix bug with caching

--- lib/balancer/servers_info.rb
+++ lib/balancer/servers_info.rb
@@ -18,7 +18,7 @@ class Balancer::ServersInfo
 
   def fetch_single_server(url)
     wrap_thread_timeout do
-      @info[url] = wrap_cache("servers_info/#{url}") do
+      @info[url] = wrap_caching("servers_info/#{url}") do
         params = "salt=#{Figaro.env.nimble_streamer_salt}&hash=#{Figaro.env.nimble_streamer_hash}"
         uri = URI("http://#{url}:8082/manage/server_status?#{params}")
         res = Net::HTTP.get_response(uri)
